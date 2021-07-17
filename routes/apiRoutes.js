31281e49b2dc108187cd92eccde0e2bc16b6c782
Buney <dpkinser93@gmail.com>
2021-07-16 18:23:11 -0600

    delete route added

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,11 +2,14 @@ const { json } = require('body-parser');
 const fs = require('fs');
 const uniqid = require('uniqid');
 const path = require('path');
-const notes = require('../db/db.json');
+
 
 module.exports = (app) => {
 
-    app.get('/api/notes', (req, res) => res.json(notes));
+    app.get('/api/notes', (req, res) => {
+        const notes = require('../db/db.json');
+        res.json(notes)
+    });
 
     // Response to post request
     app.post('/api/notes', (req, res) => {
@@ -15,22 +18,57 @@ module.exports = (app) => {
             ...req.body,
             id: uniqid(),
         };
+        // Read the db json and apply to a variable
+        const notesArr = readData();
 
-        let notesArr;
-        try {
-            const data = fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf-8');
-            notesArr = JSON.parse(data);
-        } catch (err) {
-            console.log('Trouble parsing JSON', err)
-        }
-
+        // Put the new note into the array
         notesArr.push(newNote);
-        console.log(notesArr);
-        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notesArr, null, 2), err => {
-            err ? res.json(err) : res.json(notes);
-        });
+        
+        try {
+            saveData(notesArr);
+            res.json(notesArr);
+        } catch (error) {
+            console.log(error);
+            res.json(error);
+        }
+    });
 
+    app.delete('/api/notes/:id', (req, res) => {
+        // Get the 'id' parameter from the request
+        const noteToDeleteID = req.params.id;
+        // Read the data from file
+        const notesArr = readData();
+        // Loop through the data and remove any elements with a matching id
+        for (let i = 0; i < notesArr.length; i++) {
+            if(notesArr[i].id === noteToDeleteID) {
+                notesArr.splice(i, 1);
+            }
+        }
+        // Save new array to file
+        saveData(notesArr);
+        // Respond with the notes array
+        res.json(notesArr);
     });
 
     
+}
+
+const readData = () => {
+    try {
+        const data = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf-8'));
+        return data;
+    } catch (err) {
+        console.log('Trouble parsing JSON', err);
+        return err;
+    }
+}
+
+const saveData = (array) => {
+    try {
+        fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(array, null, 2));
+        return true;
+    } catch (err) {
+        console.log('Trouble saving JSON', err);
+        return err;
+    }
 }
